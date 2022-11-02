5a202c86e77a97f67ab047e044771a39ca3c13e9
JH-UCLouvain <jeremy.holodiline@student.uclouvain.be>
2022-11-02 09:58:41 +0000

    test1 improve

--- test1.py
+++ test1.py
@@ -9,14 +9,15 @@ import jc
 
 def testPing(src, dst, net):
     resp = net[src].cmd(f"ping -6 -c 1 -W 1 {dst}")
+    print(resp)
     try:
         resp = jc.parse('ping', resp)
+        print(resp)
         if resp["packet_loss_percent"] == 0.0 and len(resp["responses"]) > 0 and resp["responses"][0]["response_ip"] == dst:
             print("sucess ping")
         else:
             print("error ping")
-    except:
-        print("error jc parsing")
+    except Exception as e: print(e)
 
 class MyTopology(IPTopo):
 
@@ -28,7 +29,7 @@ class MyTopology(IPTopo):
         self.addLink(a, b)
 
         super(MyTopology, self).build(*args, **kwargs)
-            
+
 
 net = IPNet(topo=MyTopology(), allocate_IPs=False)
 try:
@@ -37,7 +38,7 @@ try:
 
     # a ip -6 addr 2001:db8:1341:1::a/64 dev a-eth0
     # b ip -6 addr 2001:db8:1341:1::b/64 dev b-eth0
-    
+
     testPing("a", "2001:db8:1341:1::a", net)
     testPing("b", "2001:db8:1341:1::b", net)
 
